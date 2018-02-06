18651795524c8770a72c98950bf7ed06efb12e56
Muthuraj Mariappan <U325666@danfoss.com>
2018-02-06 16:20:52 +0530

    added sample workflow to the graph

--- src/components/WorkflowJsContentComponent/index.js
+++ src/components/WorkflowJsContentComponent/index.js
@@ -13,7 +13,7 @@ export class WorkflowJsContentComponent extends PureComponent {
     componentDidMount(){
       const paper = new joint.dia.Paper({
       			el: ReactDOM.findDOMNode(this.refs.placeholder),
-      			width: 600,
+      			width: 1000,
       			height: 200,
       			model: this.graph,
       			gridSize: 1
@@ -41,7 +41,61 @@ export class WorkflowJsContentComponent extends PureComponent {
           '.marker-target': { fill: 'yellow', d: 'M 10 0 L 0 5 L 10 10 z' }
       });
 
-      this.graph.addCells([rect, rect2, link]);
+      const start = new joint.shapes.basic.Ellipse({
+          position: { x: 100, y: 100 },
+          size: { width: 100, height: 30 },
+          attrs: {
+              rect: { fill: 'green' },
+              text: { text: 'start' }
+          }
+      });
+
+      const block1 = new joint.shapes.basic.Rect({
+          position: { x: 300, y: 100 },
+          size: { width: 100, height: 30 },
+          attrs: {
+              rect: { fill: 'blue' },
+              text: { text: 'Block 1', fill: 'white' }
+          }
+      });
+
+      const block2 = new joint.shapes.basic.Rect({
+          position: { x: 500, y: 100 },
+          size: { width: 100, height: 30 },
+          attrs: {
+              rect: { fill: 'blue' },
+              text: { text: 'Block 2', fill: 'white' }
+          }
+      });
+
+      const end = new joint.shapes.basic.Ellipse({
+          position: { x: 700, y: 100 },
+          size: { width: 100, height: 30 },
+          attrs: {
+              rect: { fill: 'red' },
+              text: { text: 'end' }
+          }
+      });
+
+      const link1 = new joint.dia.Link({
+          source: { id: start.id },
+          target: { id: block1.id },
+          attrs: {}
+      });
+
+      const link2 = new joint.dia.Link({
+          source: { id: block1.id },
+          target: { id: block2.id },
+          attrs: {}
+      });
+
+      const link3 = new joint.dia.Link({
+          source: { id: block2.id },
+          target: { id: end.id },
+          attrs: {}
+      });
+
+      this.graph.addCells([rect, rect2, link, start, block1, block2, end, link1, link2, link3]);
       // this.graph.addCells(this.cells);
     }
 
